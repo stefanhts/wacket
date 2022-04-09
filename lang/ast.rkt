76c4c17c2d0099bf069063c8da6fe2e9a3edd678
Stefan H <stefanh@Stefans-MacBook-Pro.local>
2022-04-09 17:16:32 -0400

    compiling abscond

---
+++ lang/ast.rkt
@@ -0,0 +1,4 @@
+#lang racket
+(provide Int)
+
+(struct Int (i) #:prefab)
---
+++ lang/compile-file.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(provide main)
+(require "parse.rkt" "compile.rkt" "../wat/printer.rkt")
+
+(define (main fn)
+    (let ((p (open-input-file fn)))
+    (begin
+    (read-line p)
+    (wat-display (compile (parse (read p))))
+    (close-input-port p)))
+)
+
