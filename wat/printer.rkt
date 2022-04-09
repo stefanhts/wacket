b24ccc145c618496d60e6ccc503333862eb0dbe4
Stefan H <stefanh@Stefans-MacBook-Pro.local>
2022-04-09 14:47:14 -0400

    parse-funcsig

--- wat/printer.rkt
+++ wat/printer.rkt
@@ -34,9 +34,19 @@
             (parse-start f ntabs)
             (parse-definitions ds ntabs))]))
 
-(define (parse-import m f fs ntabs) "TODO: parse-import")
+(define (parse-import m f fs ntabs) 
+    (match (list m f)
+        ['() (error "parse error: import")]
+        [(list '() '() '()) (error "parse error: missing names")]
+        [(list m f fs) (string-append
+
+        )]
+
+    )
+)
 (define (parse-export n d ntabs) "TODO: parse-export")
 (define (parse-func s ls b ntabs) "TODO: parse-func")
+(define (parse-funcsig n ps r) "TODO: implement this")
 
 (define (parse-start f ntabs)
     (string-append 
