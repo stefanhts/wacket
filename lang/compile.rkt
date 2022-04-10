b96b6136ea8be3df0ad1fce8c6cd907acc8d8d1e
Stefan H <stefanh@Stefans-MacBook-Pro.local>
2022-04-09 20:43:54 -0400

    making jose prettier

--- lang/compile.rkt
+++ lang/compile.rkt
@@ -5,7 +5,7 @@
         (Module
             (list
                 (Export 'main (ExportFuncSignature 'main))
-                (Func (FuncSignature 'main '() (Result (i64))) '() (Body (compile-e e)))
+                (Func (FuncSignature 'main '() (Result (i64))) '() (Body (list (compile-e e))))
             ) 
         
     )
@@ -13,13 +13,13 @@
 
 (define (compile-e e)
     (match e
-        [(Int n) (list (Const n))]
+        [(Int n) (Const n)]
         [(Prim1 p e) (compile-prim1 p e)])
 )
 
 (define (compile-prim1 p e)
     [match p
-        ('add1 (list (BiInst 'i64.add (compile-e e) (Const 1))))
-        ('sub1 (list (BiInst 'i64.sub (compile-e e) (Const 1)))) 
+        ('add1 (BiInst 'i64.add (compile-e e) (Const 1)))
+        ('sub1 (BiInst 'i64.sub (compile-e e) (Const 1))) 
     ]
 )
