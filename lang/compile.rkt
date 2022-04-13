54affd8b0cae28c0ef645ae916c2f5b3c05e8e8f
stefanhts <stefan.heller00@gmail.com>
2022-04-12 21:16:11 -0400

    char->integer and char?

--- lang/compile.rkt
+++ lang/compile.rkt
@@ -2,23 +2,16 @@
 (require "ast.rkt" "../wat/ast.rkt" "types.rkt")
 (provide compile)
 (define (compile e)
-        (Module
-            (list
-                (Export 'main (ExportFuncSignature 'main))
-                (Func (FuncSignature 'main '() (Result (i64))) '() (Body (seq (compile-e e))))
-            ) 
-        
-    )
-)
+        (Module (list (Export 'main (ExportFuncSignature 'main)) 
+                      (Func (FuncSignature 'main '() (Result (i64))) '() 
+                        (Body (seq (compile-e e)))))))
 
 (define (compile-e e)
     (match e
         [(Int n) (Const (imm->bits n))]
         [(Bool b) (Const (imm->bits b))]
         [(Prim1 p e) (compile-prim1 p e)]
-        [(If e1 e2 e3) (compile-if e1 e2 e3)]
-    )
-)
+        [(If e1 e2 e3) (compile-if e1 e2 e3)]))
 
 (define (compile-prim1 p e)
     (match p
@@ -31,11 +24,11 @@
         ['char?
             (WatIf (Eqz (Xor (And (compile-e p) (Const mask-char)) (Const type-char)))
                 (Const val-true) 
-                (Const val-false)
-            )
-        ]
-    )
-)
+                (Const val-false))]
+        ['char->integer
+            (seq (Sal (Sar (compile-e e) char-shift) int-shift))]
+        ['integer->char
+            (Xor (Sal (Sar (compile-e e) int-shift) char-shift) type-char)]))
 
 ;; Expr Expr Expr -> Asm
 (define (compile-if e1 e2 e3)
