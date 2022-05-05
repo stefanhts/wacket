93d952269b25bc224487db543da3b40d622bed2e
Peter Geertsema <peterjgeertsema@gmail.com>
2022-05-04 20:54:30 -0400

    feat(parse): add support for single let and var

--- lang/parse.rkt
+++ lang/parse.rkt
@@ -8,10 +8,13 @@
     [(? integer?) (Int s)]
     [(? boolean?) (Bool s)]
     [(? char?)    (Char s)]
+    [(? symbol?)  (Var s)]
     [(list (? op1? o) e)  (Prim1 o (parse e))]
     [(list (? op2? o) e1 e2) (Prim2 o (parse e1) (parse e2))]
     [(list 'if e1 e2 e3)
       (If (parse e1) (parse e2) (parse e3))]
+    [(list 'let (list (list (? symbol? x) e1)) e2)
+      (Let x (parse e1) (parse e2))]
     [_ (error "Parse error")]))
   
 ;; Any -> Boolean
